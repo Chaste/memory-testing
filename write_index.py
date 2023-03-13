b1492d3d46838babe0c461977ae1803f6d425a02
Fergus Cooper <fcooper8472@gmail.com>
2023-03-13 17:18:45 +0000

    Add first draft of python script to generate index

---
+++ write_index.py
@@ -0,0 +1,56 @@
+import pathlib
+import dominate
+
+from datetime import datetime
+from dominate.tags import *
+
+
+def get_list_of_log_file_directories():
+
+    base_dir = pathlib.Path('log-files')
+
+    dirs_list = [path.name for path in base_dir.iterdir() if path.is_dir()]
+    return sorted(dirs_list, reverse=True)
+
+
+def write_index_file(list_of_logs):
+
+    doc = dominate.document(title='Index of Valgrind Memcheck output')
+
+    dates = [datetime.strptime(x, '%Y-%m-%d_%H-%M-%S') for x in list_of_logs]
+
+    unique_dates = {datetime(year=date.year, month=date.month, day=1) for date in dates}
+    unique_dates = sorted(list(unique_dates), reverse=True)
+    print(unique_dates)
+
+
+
+    with doc.head:
+        link(rel='stylesheet', href='style.css')
+
+    with doc:
+
+        with div(id='title'):
+            h1('Index of Valgrind Memcheck output')
+
+        with div(id='list-by-month'):
+            attr(cls='body')
+            
+            for unique_date in unique_dates:
+                h1(unique_date.strftime("%B %Y"))
+
+                with ul():
+
+
+                    for path, date in zip(list_of_logs, dates):
+                        if date.year == unique_date.year and date.month == unique_date.month:
+                            li(a(path, href=f'{path}/index.html'))
+    
+    with open('log-files/index.html', 'w') as html_file:
+        html_file.write(doc.render())
+
+
+if __name__ == "__main__":
+
+    list_of_logs = get_list_of_log_file_directories()
+    write_index_file(list_of_logs)
