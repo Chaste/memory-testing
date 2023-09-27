ccf104a90d3d0eef19b5e9ff46bd37a72f6f41f1
Fergus Cooper <fcooper8472@gmail.com>
2023-09-27 17:56:16 +0100

    Add colours and change to monospace font

--- write_index.py
+++ write_index.py
@@ -19,6 +19,57 @@ def get_list_of_log_file_directories() -> list[str]:
     return sorted(dirs_list, reverse=True)
 
 
+def get_summary_numbers(file_content: str, regex: re.Pattern):
+    """
+    Extract and return the summary numbers for green, orange, and red statuses from the file content.
+
+    Args:
+        file_content: The content of the file as a string.
+        regex: A compiled regex that matches the green, orange, and red counts.
+    Returns:
+        A tuple containing the counts for green, orange, and red statuses respectively.
+    """
+    match = regex.search(file_content)
+    if match and len(match.groups()) == 3:
+        return int(match.group(1)), int(match.group(2)), int(match.group(3))
+
+    green_count = file_content.count('No leaks found')
+    orange_count = file_content.count('Possible leak found')
+    red_count = file_content.count('(RED)')
+
+    return green_count, orange_count, red_count
+
+
+def get_overall_colour(green: int, orange: int, red: int) -> str:
+    """
+    Determine the overall color based on the counts of green, orange, and red statuses.
+
+    Args:
+        green: Count of green statuses.
+        orange: Count of orange statuses.
+        red: Count of red statuses.
+
+    Returns:
+        A string containing the dominating colour for the table row background, prioritising red, then orange, then
+        green, and defaulting to white in the unlikely event of no recognised statuses.
+    """
+    # if red > 0:
+    #     return '#FFADAD'
+    # if orange > 0:
+    #     return '#FFD6A5'
+    # if green > 0:
+    #     return '#CAFFBF'
+    # return '#FFFFFF'
+
+    if red > 0:
+        return 'red'
+    if orange > 0:
+        return 'orange'
+    if green > 0:
+        return 'green'
+    return 'white'
+
+
 def write_index_file(list_of_logs: list[str]) -> None:
     """Write an index.html file containing hyperlinks to the log file directories contained in this directory.              
 
@@ -37,11 +88,18 @@ def write_index_file(list_of_logs: list[str]) -> None:
     unique_dates = sorted(list(unique_dates), reverse=True)
 
     # Regex to find the branch name and commit hash for this memtest output
-    pattern = r'<h2>Memtest output for commit <a href="https:\/\/github\.com\/Chaste\/Chaste\/commit\/(\w+)">\1<\/a> on branch (\w+)<\/h2>'
-    regex = re.compile(pattern)
+    header_pattern = r'<h2>Memtest output for commit <a href="https:\/\/github\.com\/Chaste\/Chaste\/commit\/(\w+)">\1<\/a> on branch (\w+)<\/h2>'
+    header_regex = re.compile(header_pattern)
+
+    # Regex to find the number of green, orange and red tests in a given file
+    summary_pattern = r'green: (\d+).*orange: (\d+).*red: (\d+)'
+    summary_regex = re.compile(summary_pattern)
 
     with doc.head:
         link(rel='stylesheet', href='style.css')
+        link(rel='preconnect', href='https://fonts.googleapis.com')
+        link(rel='preconnect', href='https://fonts.gstatic.com')
+        link(rel='stylesheet', href='https://fonts.googleapis.com/css2?family=Inconsolata&display=swap')
 
     with doc:
 
@@ -59,10 +117,15 @@ def write_index_file(list_of_logs: list[str]) -> None:
                         if date.year == unique_date.year and date.month == unique_date.month:
 
                             with open(f'log-files/{path}/index.html', 'r') as index_file:
-                                match = regex.search(index_file.read())
+                                file_content = index_file.read()
 
-                            with tr() as table_row:
+                            match = header_regex.search(file_content)
+                            green, orange, red = get_summary_numbers(file_content, summary_regex)
+                            colour = get_overall_colour(green, orange, red)
+
+                            with tr(cls=f'tr-{colour}') as table_row:
                                 table_row.add(td(a(path, href=f'{path}/index.html')))
+                                table_row.add(td(f'{colour}'))
                                 if match:
                                     table_row.add(td(match.group(2)))
                                     table_row.add(td(a(match.group(1), href=f'https://github.com/Chaste/Chaste/commit/{match.group(1)}')))
